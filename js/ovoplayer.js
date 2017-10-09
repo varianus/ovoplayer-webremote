e91347539d0a37d2818dbe33cd7a1fa7cff78a2f
varianus <varianus@27520a1b-e1ea-44b1-99aa-068e44c1cd7b>
2017-10-09 14:29:06 +0000

    Initial commit

---
+++ js/ovoplayer.js
@@ -0,0 +1,69 @@
+function webSocketConnect() {
+  if (typeof MozWebSocket != 'undefined') {
+    socket = new MozWebSocket(get_appropriate_ws_url())
+  } else {
+    socket = new WebSocket(get_appropriate_ws_url())
+  }
+
+  try {
+    socket.onopen = function () {
+      connected()
+    }
+    socket.onmessage = function (msg) {
+      handle_message(msg)
+    }
+    socket.onclose = function () {
+      console.log('disconnected')
+    }
+  } catch (exception) {
+    alert('<p>Error' + exception)
+  }
+}
+
+function connected() {
+  console.log('connected')
+  socket.send('xxxxcfg:keep')
+  socket.send('xxxxcfg:autopos')
+}
+
+function get_appropriate_ws_url() {
+  return 'ws://127.0.0.1:6860/player'
+}
+
+function decodeOvoLength(base64) {
+  var binary_string = window.atob(base64);
+  var len = binary_string.length;
+  var num = 0;
+  for (var i = 0; i < len; i++) {
+    num = num | (binary_string.charCodeAt(i) << (len - i -1 ) * 8);
+  }
+  return num
+}
+
+var ovoCommand = function () {
+  this.size = 0
+  this.category = ""
+  this.command = ""
+  this.param = ""
+}
+
+function split_message(msg) {
+  var obj = new ovoCommand();
+  obj.size = decodeOvoLength(msg.substr(0, 4))
+  obj.category = msg.slice(4, msg.indexOf(":"))
+  obj.command = msg.slice(msg.indexOf(":")+1, msg.indexOf("="))
+  obj.param = msg.slice(msg.indexOf("=")+1)
+  return obj
+
+}
+
+function handle_message(msg) {
+  if (msg.data.length == 0)
+    return;
+  console.log(msg.data)
+
+  message = split_message(msg.data)
+  console.log(message)
+
+
+}
