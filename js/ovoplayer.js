de6c5663330ace09a77297c2a0b51b8370739308
varianus <varianus@27520a1b-e1ea-44b1-99aa-068e44c1cd7b>
2017-10-18 12:55:25 +0000

    Show current song on playlist Play a song by clicking on playlist

--- js/ovoplayer.js
+++ js/ovoplayer.js
@@ -1,272 +1,284 @@
- function init() {
-   var server = localStorage.getItem('server')
-   var port = localStorage.getItem('port')
-   if (!server) {
-     server = '127.0.0.1'
-     localStorage.setItem('server', server)
-   }
-   if (!port) {
-     port = '6860'
-     localStorage.setItem('port', port)
-   }
-   webSocketConnect(server, port);
+function init() {
+  var server = localStorage.getItem('server')
+  var port = localStorage.getItem('port')
+  if (!server) {
+    server = '127.0.0.1'
+    localStorage.setItem('server', server)
+  }
+  if (!port) {
+    port = '6860'
+    localStorage.setItem('port', port)
+  }
+  webSocketConnect(server, port)
+}
 
- }
+function byId(id) {
+  return document.getElementById(id)
+}
 
+function toast(message) {
+  var x = byId('toast')
+  x.innerText = message
+  x.className = 'show'
+  setTimeout(function () {
+    x.className = x.className.replace('show', '')
+  }, 4000)
+}
 
- function byId(id) {
-   return document.getElementById(id)
- }
+function webSocketConnect(server, port) {
+  if (typeof MozWebSocket != 'undefined') {
+    socket = new MozWebSocket(get_appropriate_w_url(server, port))
+  } else {
+    socket = new WebSocket(get_appropriate_w_url(server, port))
+  }
 
- function toast(message) {
-   var x = byId("toast")
-   x.innerText = message
-   x.className = "show";
-   setTimeout(function() { x.className = x.className.replace("show", ""); }, 4000);
- }
+  try {
+    socket.onopen = function () {
+      toast('Connected')
+      connected()
+    }
+    socket.onmessage = function (msg) {
+      handle_message(msg)
+    }
+    socket.onclose = function () {
+      toast('Disconnected')
+      console.log('disconnected')
+    }
+  } catch (exception) {
+    alert('<p>Error' + exception)
+  }
+}
 
- function webSocketConnect(server, port) {
-   if (typeof MozWebSocket != 'undefined') {
-     socket = new MozWebSocket(get_appropriate_ws_url(server, port))
-   } else {
-     socket = new WebSocket(get_appropriate_ws_url(server, port))
-   }
+function sendCommand(category, command, params) {
+  var msg = category + ':' + command
+  if (params)
+    msg += '=' + params
 
-   try {
-     socket.onopen = function() {
-       toast('Connected')
-       connected()
-     }
-     socket.onmessage = function(msg) {
-       handle_message(msg)
-     }
-     socket.onclose = function() {
-       toast('Disconnected')
-       console.log('disconnected')
-     }
-   } catch (exception) {
-     alert('<p>Error' + exception)
-   }
- }
+  msg = encodeOvoLength(msg.length) + msg
 
- function sendCommand(category, command, params) {
-   var msg = category + ':' + command
-   if (params)
-     msg += '=' + params
+  socket.send(msg)
+}
 
-   msg = encodeOvoLength(msg.length) + msg
+function connected() {
+  console.log('connected')
+  sendCommand('cfg', 'keep')
+  sendCommand('cfg', 'autopos')
+  sendCommand('cfg', 'size', 1)
+  sendCommand('req', 'state')
+  sendCommand('req', 'meta')
+  sendCommand('req', 'coverimg')
+  sendCommand('req', 'vol')
+}
 
-   socket.send(msg)
- }
+function get_appropriate_w_url(server, port) {
+  return 'ws://' + server + ':' + port + '/player'
+}
 
- function connected() {
-   console.log('connected')
-   sendCommand('cfg', 'keep')
-   sendCommand('cfg', 'autopos')
-   sendCommand('cfg', 'size', 1)
-   sendCommand('req', 'state')
-   sendCommand('req', 'meta')
-   sendCommand('req', 'coverurl')
-   sendCommand('req', 'vol')
- }
+function decodeOvoLength(base64) {
+  var binary_string = window.atob(base64)
+  var len = binary_string.length
+  var num = 0
+  for (var i = 0; i < len; i++) {
+    num = num | (binary_string.charCodeAt(i) << (len - i - 1) * 8)
+  }
+  return num
+}
 
+function encodeOvoLength(len) {
+  var binary_string = ''
+  for (var i = 0; i < 3; i++) {
+    binary_string += String.fromCharCode(len >> ((2 - i) * 8) & 0xff)
+  }
+  return btoa(binary_string)
+}
 
+var ovoCommand = function () {
+  this.size = 0
+  this.category = ''
+  this.command = ''
+  this.param = ''
+}
 
+var ovoMeta = function () {
+  this.ID = 0
+  this.FileName = ''
+  this.Album = ''
+  this.AlbumArtist = ''
+  this.Artist = ''
+  this.Comment = ''
+  this.Duration = 0
+  this.Genre = ''
+  this.Title = ''
+  this.TrackString = ''
+  this.Year = ''
+}
 
+function decodeMeta(meta) {
+  var obj = new ovoMeta()
+  var startpos = 4
 
- function get_appropriate_ws_url() {
-   return 'ws://127.0.0.1:6860/player'
- }
+  function extractfield() {
+    len = decodeOvoLength(meta.substr(startpos, 4))
+    startpos += 4
+    var oldpos = startpos
+    startpos += len
+    return meta.substr(oldpos, len)
+  }
 
- function decodeOvoLength(base64) {
-   var binary_string = window.atob(base64)
-   var len = binary_string.length
-   var num = 0
-   for (var i = 0; i < len; i++) {
-     num = num | (binary_string.charCodeAt(i) << (len - i - 1) * 8)
-   }
-   return num
- }
+  obj.ID = extractfield()
+  obj.FileName = extractfield()
+  obj.Album = extractfield()
+  obj.AlbumArtist = extractfield()
+  obj.Artist = extractfield()
+  obj.Comment = extractfield()
+  obj.Duration = extractfield()
+  obj.Genre = extractfield()
+  obj.Title = extractfield()
+  obj.TrackString = extractfield()
+  obj.Year = extractfield()
+  return obj
+}
 
- function encodeOvoLength(len) {
-   var binary_string = ''
-   for (var i = 0; i < 3; i++) {
-     binary_string += String.fromCharCode(len >> ((2 - i) * 8) & 0xff)
-   }
-   return btoa(binary_string)
- }
+function decodePlayList(meta) {
+  var startpos = 4
+  var objs = []
 
- var ovoCommand = function() {
-   this.size = 0
-   this.category = ''
-   this.command = ''
-   this.param = ''
- }
+  var totlen = decodeOvoLength(meta.substr(0, 4))
+  var count = meta.substr(4, totlen) * 1
+  startpos += totlen
 
- var ovoMeta = function() {
-   this.ID = 0
-   this.FileName = ''
-   this.Album = ''
-   this.AlbumArtist = ''
-   this.Artist = ''
-   this.Comment = ''
-   this.Duration = 0
-   this.Genre = ''
-   this.Title = ''
-   this.TrackString = ''
-   this.Year = ''
- }
+  while (count > 0) {
+    var len = decodeOvoLength(meta.substr(startpos, 4))
+    objs.push(decodeMeta(meta.substr(startpos, len)))
+    startpos += (len + 4)
+    count -= 1
+  }
+  return objs
+}
 
- function decodeMeta(meta) {
-   var obj = new ovoMeta()
-   var startpos = 4
+function split_message(msg) {
+  var obj = new ovoCommand()
+  obj.size = decodeOvoLength(msg.substr(0, 4))
 
-   function extractfield() {
-     len = decodeOvoLength(meta.substr(startpos, 4))
-     startpos += 4
-     var oldpos = startpos
-     startpos += len
-     return meta.substr(oldpos, len)
-   }
+  var colon = msg.indexOf(':')
+  obj.category = msg.slice(4, colon)
+  var pos = msg.indexOf('=')
+  if (pos > -1) {
+    obj.command = msg.slice(colon + 1, pos)
+    obj.param = msg.slice(pos + 1)
+  } else {
+    obj.command = msg.slice(colon + 1)
+  }
 
-   obj.ID = extractfield()
-   obj.FileName = extractfield()
-   obj.Album = extractfield()
-   obj.AlbumArtist = extractfield()
-   obj.Artist = extractfield()
-   obj.Comment = extractfield()
-   obj.Duration = extractfield()
-   obj.Genre = extractfield()
-   obj.Title = extractfield()
-   obj.TrackString = extractfield()
-   obj.Year = extractfield()
-   return obj
- }
+  return obj
+}
 
- function decodePlayList(meta) {
-   var startpos = 4
-   var objs = []
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = parseInt((duration / 1000) % 60),
+    minutes = parseInt((duration / (1000 * 60)) % 60),
+    hours = parseInt((duration / (1000 * 60 * 60)) % 24)
 
-   var totlen = decodeOvoLength(meta.substr(0, 4))
-   var count = meta.substr(4, totlen) * 1
-   startpos += totlen
+  if (hours > 0) {
+    hours = hours + ':'
+    minutes = (minutes < 10) ? '0' + minutes : minutes
+  } else {
+    hours = ''
+  }
+  seconds = (seconds < 10) ? '0' + seconds : seconds
 
-   while (count > 0) {
-     var len = decodeOvoLength(meta.substr(startpos, 4))
-     objs.push(decodeMeta(meta.substr(startpos, len)))
-     startpos += (len + 4)
-     count -= 1
-   }
-   return objs
- }
+  return hours + minutes + ':' + seconds
+}
 
- function split_message(msg) {
-   var obj = new ovoCommand()
-   obj.size = decodeOvoLength(msg.substr(0, 4))
+function handle_message(msg) {
+  if (msg.data.length == 0)
+    return
 
-   var colon = msg.indexOf(':')
-   obj.category = msg.slice(4, colon)
-   var pos = msg.indexOf('=')
-   if (pos > -1) {
-     obj.command = msg.slice(colon + 1, pos)
-     obj.param = msg.slice(pos + 1)
-   } else {
-     obj.command = msg.slice(colon + 1)
-   }
+  message = split_message(msg.data)
 
-   return obj
- }
+  switch (message.category) {
+    case 'info':
+      switch (message.command) {
+        case 'pos':
+          byId('songpos').value = message.param
+          byId('textPos').innerText = msToTime(message.param)
+          break
+        case 'vol':
+          byId('volume').value = message.param
+          break
+        case 'meta':
+          var meta = decodeMeta(message.param)
+          byId('songpos').max = meta.Duration
+          byId('title').innerText = meta.Title
+          byId('artist').innerText = meta.Artist
+          byId('album').innerText = meta.Album
+          byId('textDuration').innerText = msToTime(meta.Duration)
+          break
+        case 'coverurl':
+        case 'coverimg':
+          if (message.param === "")
+            byId('cover').src = 'asset/noconver.png'
+          else
+            byId('cover').src = message.param
+          break
+        case 'playlist':
+          var playlist = decodePlayList(message.param)
+          var tableObj = byId('pl-data')
+          tableObj.innerText = ''
+          for (var i = 0; i < playlist.length; i++) {
+            var row = tableObj.insertRow(-1)
+            var c0 = row.insertCell(0)
+            c0.innerText = playlist[i].Title
+            var c1 = row.insertCell(1)
+            c1.innerText = playlist[i].Artist
+            var c2 = row.insertCell(2)
+            c2.innerText = msToTime(playlist[i].Duration)
+            row.onclick = (function () {
+              return function () {
+                sendCommand('action', 'play', this.rowIndex - 1)
+              }
+            })()
+          }
+          break
+        case 'state':
+          byId('playbtn').classList.remove('ico-play', 'ico-pause')
+          switch (message.param) {
+            case '0':
+              byId('plstate').className = 'ico-stop'
+              byId('playbtn').classList.add('ico-play')
+              break
+            case '1':
+              byId('plstate').className = 'ico-play'
+              byId('playbtn').classList.add('ico-pause')
+              sendCommand('req', 'meta')
+              sendCommand('req', 'coverimg')
+              break
+            case '2':
+              byId('plstate').className = 'ico-stop'
+              byId('playbtn').classList.add('ico-play')
+              break
 
- function msToTime(duration) {
-   var milliseconds = parseInt((duration % 1000) / 100),
-     seconds = parseInt((duration / 1000) % 60),
-     minutes = parseInt((duration / (1000 * 60)) % 60),
-     hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+          }
+          break
+        case 'index':
+          var trele = byId('tabpl').getElementsByTagName('TR')
+          for (var i = 0; i < trele.length; i++) {
+            trele[i].classList.remove('selected')
+          }
+          if (trele.length > 1) {
+            trele[message.param].classList.add('selected')
+            // trele[message.param].scrollIntoView([])
+          }
+          break
+      }
+      break
+  }
+}
 
-   if (hours > 0) {
-     hours = hours + ":"
-     minutes = (minutes < 10) ? "0" + minutes : minutes;
-   } else {
-     hours = ""
-   }
-   seconds = (seconds < 10) ? "0" + seconds : seconds;
+function setVolume() {
+  sendCommand('action', 'vol', byId('volume').value)
+}
 
-   return hours + minutes + ":" + seconds;
- }
-
- function handle_message(msg) {
-   if (msg.data.length == 0)
-     return
-
-   message = split_message(msg.data)
-
-   switch (message.category) {
-     case 'info':
-       switch (message.command) {
-         case 'pos':
-           byId('songpos').value = message.param
-           byId('textPos').innerText = msToTime(message.param)
-           break
-         case 'vol':
-           byId('volume').value = message.param
-           break
-         case 'meta':
-           var meta = decodeMeta(message.param)
-           byId('songpos').max = meta.Duration
-           byId('title').innerText = meta.Title
-           byId('artist').innerText = meta.Artist
-           byId('album').innerText = meta.Album
-           byId('textDuration').innerText = msToTime(meta.Duration)
-           break
-         case 'coverurl':
-           byId('cover').src = message.param
-           break
-         case 'coverimg':
-           byId('cover').src = 'data:image/jpeg;base64, ' + message.param
-           break
-         case 'playlist':
-           var playlist = decodePlayList(message.param)
-           var tableObj = byId('pl-data')
-           tableObj.innerText = ''
-           for (var i = 0; i < playlist.length; i++) {
-             var row = tableObj.insertRow(-1)
-             var c0 = row.insertCell(0)
-             c0.innerText = playlist[i].Title
-             var c1 = row.insertCell(1)
-             c1.innerText = playlist[i].Artist
-             var c2 = row.insertCell(2)
-             c2.innerText = msToTime(playlist[i].Duration)
-           }
-           break
-         case 'state':
-           byId('playbtn').classList.remove("ico-play", "ico-pause")
-           switch (message.param) {
-             case "0":
-               byId('plstate').className = "ico-stop"
-               byId('playbtn').classList.add("ico-play");
-               break
-             case "1":
-               byId('plstate').className = "ico-play"
-               byId('playbtn').classList.add("ico-pause");
-               sendCommand('req', 'meta')
-               sendCommand('req', 'coverurl')
-               break
-             case "2":
-               byId('plstate').className = "ico-stop"
-               byId('playbtn').classList.add("ico-play");
-               break
-
-           }
-           break
-       }
-       break
-   }
- }
-
- function setVolume() {
-   sendCommand('action', 'vol', byId('volume').value)
- }
-
- function seek() {
-   sendCommand('action', 'seek', byId('songpos').value)
- }
+function seek() {
+  sendCommand('action', 'seek', byId('songpos').value)
+}
